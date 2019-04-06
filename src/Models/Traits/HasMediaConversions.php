dd5b3a20c7e1dc203f251dee13ec706be07bad00
Tobi Kivelip <tkivelip@gmail.com>
2019-04-06 18:14:50 +0200

    Conversions

---
+++ src/Models/Traits/HasMediaConversions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Laramate\StructuredDocument\Models\Traits;
+
+use Spatie\MediaLibrary\Models\Media;
+
+trait HasMediaConversions
+{
+    public function registerMediaCollections()
+    {
+        foreach (config('document.media_conversions') as $collection=>$conversions) {
+            $this->addMediaCollection($collection);
+        }
+    }
+
+    public function registerMediaConversions(Media $media = null)
+    {
+        foreach (config('document.media_conversions') as $collection=>$conversions) {
+            foreach ($conversions as $conversionKey => $config) {
+                $conversion = $this->addMediaConversion($conversionKey)
+                    ->performOnCollections($collection);
+
+                if (isset($config['width'])) {
+                    $conversion->width($config['width']);
+                }
+
+                if (isset($config['height'])) {
+                    $conversion->width($config['height']);
+                }
+
+                if (isset($config['crop'])) {
+                    $conversion->crop($config['crop'], $config['width'], $config['height']);
+                }
+            }
+        }
+    }
+}
